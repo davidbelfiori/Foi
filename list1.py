8cc38bfba8f16c1f70367f04099206f5b78f6426
davidbelfiori <belfiori.david_julian.stud@itisgalileiroma.it>
2022-11-21 17:59:06 +0100

    avevo letto male il testo

--- list1.py
+++ list1.py
@@ -1,4 +1,8 @@
-mylist = [8, 0, 2, 1, 4, 8, 9]
+#Definire una funzione inserisci(L, n1, n2) : data una lista di interi L e due interi n1 e n2,
+#la funzione restituisce una nuova lista ottenuta da L (senza modificare L) dove il valore n2 viene
+#inserito subito dopo ogni occorrenza del valore n1.Esempio: inserisci ([3, 12, 4, 3, 10, 3], 3, 0)
+#produce come risultato la lista [3, 0, 12, 4, 3, 0, 10, 3, 0]
+mylist = [3, 12, 4, 3, 10, 3]
 
 
 def ins(mylist, num1, num2):
@@ -7,9 +11,15 @@ def ins(mylist, num1, num2):
     # @param num2: int 
     s = []
     for i in range(0, len(mylist)):
-        s.append(mylist[i])
-    s.append(num1)
-    s.append(num2)
+        if mylist[i] == num1:
+            s.append(mylist[i])
+            s.append(num2)
+        else:
+
+             s.append(mylist[i])
+
+
+
     return s
 
 
